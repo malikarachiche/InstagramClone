4f11e32c3114217cf5f93c6ab37fc2809059c8b8
Malik <arachich@msu.edu>
2021-05-06 13:16:54 -0400

    Set up firebase

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Malik Arachiche on 5/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .secondarySystemBackground
+        // Do any additional setup after loading the view.
+    }
+
+}
+
